cd0ab68a0a9057d9a614b462ff00c5888746bcaf
Evilsysadmin <trainingit@zoho.com>
2026-08-17 19:41:54 +0200

    correct vite cfg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/chess-studio/',
+
   plugins: [react()],
+
   server: {
     port: 5173,
-    base: '/chess-studio/',
   },
+
   test: {
     environment: 'node',
     setupFiles: ['./src/test-setup.js'],
