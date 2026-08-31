86b4cdabdbe3af3bc43b02d19f5466dfc527c68e
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-08-31 09:18:15 +0200

    Render deployment identity in staging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import AppRootErrorBoundary from './components/AppRootErrorBoundary.jsx';
+import StagingBanner from './components/StagingBanner.jsx';
 import './styles.css';
 import { installReleaseContinuity } from './releaseContinuity.js';
 import { migratePersistentStorage } from './storageMigrations.js';
@@ -18,6 +19,7 @@ document.documentElement.dataset.motionPreference = getReducedMotionPreference()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppRootErrorBoundary>
+      <StagingBanner />
       <App />
     </AppRootErrorBoundary>
   </React.StrictMode>
