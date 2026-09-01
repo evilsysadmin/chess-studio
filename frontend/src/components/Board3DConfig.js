2b62f438e9a9d164b051a4089dc8665695f3e83b
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-01 22:38:12 +0200

    Extract Board3D configuration

---
+++ frontend/src/components/Board3DConfig.js
@@ -0,0 +1,50 @@
+export const FILES = Object.freeze(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
+export const DISPLAY_RANKS = Object.freeze(['8', '7', '6', '5', '4', '3', '2', '1']);
+
+export const BOARD_THEME_3D = Object.freeze({
+  classic: { light: 0xd9cfba, dark: 0x5a4236, frame: 0x34251f, felt: 0x111722, glow: 0xc9a227 },
+  midnight: { light: 0xaab2bd, dark: 0x263244, frame: 0x111824, felt: 0x080d16, glow: 0x6f9fc5 },
+  blood: { light: 0xc9b5a6, dark: 0x5d2926, frame: 0x2b1515, felt: 0x12090a, glow: 0xb4483a },
+  royal: { light: 0xd8c990, dark: 0x493564, frame: 0x221b32, felt: 0x0d0b15, glow: 0xe0b84e },
+  forensic: { light: 0xc6d2ce, dark: 0x40515a, frame: 0x1c262b, felt: 0x0a1013, glow: 0x63c0ba },
+  obsidian: { light: 0xb5b0a8, dark: 0x202225, frame: 0x0c0d0f, felt: 0x050607, glow: 0xc7a34a },
+});
+
+export const SKIN_3D = Object.freeze({
+  default: {
+    white: 0xe7d7ad, black: 0x2b2d31, whiteAccent: 0xb68a38, blackAccent: 0x9b342f,
+    metalness: 0.18, roughness: 0.62, emissive: 0x000000, emissiveIntensity: 0,
+  },
+  studio: {
+    white: 0xf0eadc, black: 0x262a30, whiteAccent: 0xc7a34a, blackAccent: 0x8f312e,
+    metalness: 0.26, roughness: 0.48, emissive: 0x000000, emissiveIntensity: 0,
+  },
+  regimiento: {
+    white: 0xf2e1bd, black: 0x313238, whiteAccent: 0xc79b43, blackAccent: 0xa62e2a,
+    metalness: 0.44, roughness: 0.38, emissive: 0x000000, emissiveIntensity: 0,
+  },
+  azul: {
+    white: 0xd8e2e8, black: 0x1f3344, whiteAccent: 0x6f9fc5, blackAccent: 0x366c94,
+    metalness: 0.32, roughness: 0.44, emissive: 0x000000, emissiveIntensity: 0,
+  },
+  shogunate: {
+    white: 0xe9e0cf, black: 0x162236, whiteAccent: 0xc33d45, blackAccent: 0x305ea8,
+    metalness: 0.5, roughness: 0.32, emissive: 0x142f70, emissiveIntensity: 0.12,
+  },
+  esmeralda: {
+    white: 0xd9dcc9, black: 0x23372e, whiteAccent: 0x759b68, blackAccent: 0x36634d,
+    metalness: 0.28, roughness: 0.5, emissive: 0x000000, emissiveIntensity: 0,
+  },
+  cyber: {
+    white: 0xcbd6df, black: 0x151b23, whiteAccent: 0x53b7d8, blackAccent: 0x7f3dcc,
+    metalness: 0.72, roughness: 0.24, emissive: 0x297ea7, emissiveIntensity: 0.18,
+  },
+  marines: {
+    white: 0xc9c5ad, black: 0x2d352f, whiteAccent: 0x9f8b52, blackAccent: 0x526b4e,
+    metalness: 0.34, roughness: 0.64, emissive: 0x000000, emissiveIntensity: 0,
+  },
+  delta: {
+    white: 0xbfc2c3, black: 0x151718, whiteAccent: 0x922c2a, blackAccent: 0x721f20,
+    metalness: 0.5, roughness: 0.34, emissive: 0x641414, emissiveIntensity: 0.12,
+  },
+});
