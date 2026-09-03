3bf83870043aadbbac83f3b7c04f3eb43e2e8681
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-03 16:54:47 +0200

    Wire Matthias state expression styling

--- frontend/src/components/MatthiasWarRoomPortrait.jsx
+++ frontend/src/components/MatthiasWarRoomPortrait.jsx
@@ -14,6 +14,7 @@ import {
   transitionMatthiasWarRoom,
 } from './matthiasWarRoomStateMachine.js';
 import './MatthiasWarRoomPortrait.css';
+import './MatthiasWarRoomStateExpressions.css';
 import './MatthiasWarRoomThreeAvatar.css';
 import './MatthiasWarRoomAndroidMotion.css';
 import './WarRoomReferencePolish.css';
