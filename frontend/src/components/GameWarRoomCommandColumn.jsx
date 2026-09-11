93dd8c32589ae1812c017d4c85ee82a225f76ebc
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-11 22:48:51 +0200

    polish(war-room): wire cinematic HUD layer

--- frontend/src/components/GameWarRoomCommandColumn.jsx
+++ frontend/src/components/GameWarRoomCommandColumn.jsx
@@ -8,6 +8,7 @@ import './WarRoom3DMobileControls.css';
 import './WarRoomDesktopRailLayout.css';
 import './WarRoomMatthiasDiegetic.css';
 import './WarRoomFloatingFooter.css';
+import './WarRoomCinematicHud.css';
 import './WarRoomAppearanceMenu.css';
 
 function resolveWarRoomSignal(game, status) {
