f40d11aba0cb27a5b81e8d4c32d791ae3946438a
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-15 20:41:05 +0200

    feat(oci): define console verification inputs

---
+++ infra/oci/verify/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type = string
+}
+
+variable "compartment_ocid" {
+  type = string
+}
+
+variable "instance_id" {
+  type = string
+}
+
+variable "console_public_key" {
+  type = string
+}
