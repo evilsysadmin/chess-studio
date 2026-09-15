f37c415b9fa454136203b2d22f585af9f2943866
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-15 14:22:31 +0200

    feat(chronicles): define close behind-party camera contract

---
+++ frontend/src/chroniclesTacticsCameraContract.js
@@ -0,0 +1,6 @@
+export const CHRONICLES_TACTICS_CAMERA = Object.freeze({
+  positionOffset: Object.freeze({ x: 5.3, y: 5.4, z: 6.5 }),
+  targetOffset: Object.freeze({ x: -1.0, y: 0.88, z: -1.35 }),
+  fov: 40.5,
+  partyScale: 1.11,
+});
