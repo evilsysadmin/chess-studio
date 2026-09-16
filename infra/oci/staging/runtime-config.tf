be56dc5901f9339303b626e599cd6946e006d5ef
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-16 23:46:49 +0200

    security(oci): drop unused secret-bundle read

--- infra/oci/staging/runtime-config.tf
+++ infra/oci/staging/runtime-config.tf
@@ -53,7 +53,6 @@ resource "oci_identity_policy" "staging_runtime_config" {
   description    = "Least-privilege runtime access for Chess Studio staging instances."
   statements = [
     "Allow dynamic-group ${oci_identity_dynamic_group.staging_backend.name} to read objects in compartment id ${var.compartment_ocid} where target.bucket.name='${oci_objectstorage_bucket.runtime_config.name}'",
-    "Allow dynamic-group ${oci_identity_dynamic_group.staging_backend.name} to read secret-bundles in compartment id ${var.compartment_ocid}",
     "Allow dynamic-group ${oci_identity_dynamic_group.staging_backend.name} to use instance-agent-command-execution-family in compartment id ${var.compartment_ocid} where request.instance.id=target.instance.id",
   ]
 }
