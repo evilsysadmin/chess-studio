538c97c507630540cfefec81dba2bfcc4c9aa4eb
Linux Pirate Ninja <12677236+evilsysadmin@users.noreply.github.com>
2026-09-23 22:50:33 +0200

    style(school): request classroom board framing

--- frontend/src/components/SchoolBoard.jsx
+++ frontend/src/components/SchoolBoard.jsx
@@ -6,5 +6,5 @@ export function getSchoolBoardRenderer() {
 }
 
 export default function SchoolBoard(props) {
-  return <PreferredBoard {...props} />;
+  return <PreferredBoard {...props} cameraProfile="classroom" />;
 }
